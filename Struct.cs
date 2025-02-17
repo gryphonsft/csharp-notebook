e4dc6ab22c35dae5bac9d18be8cc8f00473d9945
gryphonsft <abdbzd01@gmail.com>
2025-02-17 20:07:04 +0300

    Proje dosyası ekle.

---
+++ Struct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalısmaNotlarim
+{
+    internal class Struct
+    {
+        struct Futbolcu
+        {
+            public string isim;
+            public string soyad;
+            public string mevki;
+            public int yas;
+        }
+        static void Main(string[] args)
+        {
+            Futbolcu nesne = new Futbolcu();
+            Console.Write("Lütfen isminizi giriniz: ");
+            nesne.isim = Console.ReadLine();
+            Console.Write("Lütfen soyadınızı giriniz: ");
+            nesne.soyad = Console.ReadLine();
+            Console.Write("Lütfen mevkinizi giriniz: ");
+            nesne.mevki = Console.ReadLine();
+            Console.Write("Lütfen yaşınızı giriniz: ");
+            nesne.yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nİsminiz : " + nesne.isim);
+            Console.WriteLine("Soyadınız : " + nesne.soyad);
+            Console.WriteLine("Mevkiniz : " + nesne.mevki);
+            Console.WriteLine("Yaşınız : " + nesne.yas);
+            Console.ReadLine();
+
+        }
+    }
+}
